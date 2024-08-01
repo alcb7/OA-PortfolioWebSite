4fe8de9fd40c64aedbd0d982882f8e5015143569
alcb7 <canbulan1849@gmail.com>
2024-08-01 18:34:29 +0300

    Database configurations

---
+++ OA.PortfolioWebSite/Infrastructure/OA.PortfolioWebSite.Persistance/Contexts/AuthAPIDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA.PortfolioWebSite.Persistance.Contexts
+{
+    public class AuthAPIDbContext : DbContext
+    {
+        public AuthAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ OA.PortfolioWebSite/Infrastructure/OA.PortfolioWebSite.Persistance/ServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OA.PortfolioWebSite.Persistance.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA.PortfolioWebSite.Persistance
+{
+    //public void ServiceRegistration(IServiceCollection services)
+    //{
+    //    //services.AddDbContext<DataAPIDbContext>(options =>
+    //    //    options.UseSqlServer(Configuration.GetConnectionString("AuthConnection")));
+
+    //    services.AddDbContext<DataAPIDbContext>(options =>
+    //        options.UseSqlServer(Configuration.GetConnectionString("DataConnection")));
+
+    //    services.AddScoped<UserRepository>();
+    //    services.AddScoped<ExperienceRepository>();
+
+    //    // Diğer servis bağımlılıkları
+    //}
+    
+    
+}
