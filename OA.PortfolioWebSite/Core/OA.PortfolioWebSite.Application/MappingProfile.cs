438647d5740a36a3b390afd25697cad569ae23ce
alcb7 <canbulan1849@gmail.com>
2024-08-13 19:52:00 +0300

    Update MappingProfile.cs

--- OA.PortfolioWebSite/Core/OA.PortfolioWebSite.Application/MappingProfile.cs
+++ OA.PortfolioWebSite/Core/OA.PortfolioWebSite.Application/MappingProfile.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace OA.PortfolioWebSite.Application
 {
